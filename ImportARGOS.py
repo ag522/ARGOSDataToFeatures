0c6acd0ef24739c0802a5514db978b7dc327001e
ag522 <ag522@duke.edu>
2020-10-11 23:51:41 -0400

    Update ImportARGOS.py

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -13,9 +13,10 @@
 import sys, os, arcpy
 arcpy.env.overwriteOutput = True
 # Set input variables (Hard-wired)
-inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
-outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
-outputSR = arcpy.SpatialReference(54002)
+inputFolder = arcpy.GetParameterAsText(0)
+inputFiles = os.listdir(inputFolder)
+outputFC = arcpy.GetParameterAsText(1)
+outputSR = arcpy.GetParameterAsText(2)
 ## Prepare a new feature class to which we'll add tracking points
 # Create an empty feature class; requires the path and name as separate parameters
 outPath,outName = os.path.split(outputFC)
@@ -23,72 +24,83 @@ arcpy.CreateFeatureclass_management(outPath, outName,"POINT","","","",outputSR)
 # Add TagID, LC, IQ, and Date fields to the output feature class
 arcpy.AddField_management(outputFC,"TagID","LONG")
 arcpy.AddField_management(outputFC,"LC","TEXT")
-arcpy.AddField_management(outputFC,"Date","DATE")
+arcpy.AddField_management(outputFC,"Date","TEXT")
 # Create the insert cursor
 cur = arcpy.da.InsertCursor(outputFC,['Shape@','TagID','LC','Date'])
-#%% Construct a while loop to iterate through all lines in the datafile
-# Open the ARGOS data file for reading
-inputFileObj = open(inputFile,'r')
-
-# Get the first line of data, so we can use a while loop
-lineString = inputFileObj.readline()
-
-# Start the while loop
-while lineString:
+#Loop through each file in the ARGOS folder
+for inputFile in inputFiles:
     
-    # Set code to run only if the line contains the string "Date: "
-    if ("Date :" in lineString):
-        
-        # Parse the line into a list
-        lineData = lineString.split()
-        
-        # Extract attributes from the datum header line
-        tagID = lineData[0]
-        obsDate = lineData[3]
-        obsTime = lineData[4]
-        obsClass = lineData[7]
-        
-        # Extract location info from the next line
-        line2String = inputFileObj.readline()
-        
-        # Parse the line into a list
-        line2Data = line2String.split()
-        
-        # Extract the date we need to variables
-        obsLat = line2Data[2]
-        obsLon= line2Data[5]
-        
-        # Print results to see how we're doing
-        print (tagID,"Date:"+obsDate,"Time:"+obsTime,"Class:"+obsClass,"Lat:"+obsLat,"Long:"+obsLon)
-        #Try to convert the coordinates to numbers
-        try:
-            # Convert raw coordinate strings to numbers
-            if obsLat[-1] == 'N':
-                obsLat = float(obsLat[:-1])
-            else:
-                obsLat = float(obsLat[:-1]) * -1
-            if obsLon[-1] == 'E':
-                obsLon = float(obsLon[:-1])
-            else:
-                obsLon = float(obsLon[:-1]) * -1
-            # Construct a point object from the feature class
-            obsPoint = arcpy.Point()
-            obsPoint.X = obsLon
-            obsPoint.Y = obsLat
-            # Convert the point to a point geometry object with spatial reference
-            inputSR = arcpy.SpatialReference(4326)
-            obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
-            
-            # Create a feature object
-            feature = cur.insertRow((obsPointGeom,tagID,obsClass,obsDate.replace(".","/") + " " + obsTime))
-        #Handle any error
-        except Exception as e:
-            print(f"Error adding record {tagID} to the output")
-      
-    # Move to the next line so the while loop progresses
+    #Give some status
+    print("Processing {}".format(inputFile))
+    #Skip the README.txt file
+    if inputFile == 'README.txt': continue
+    #Append the path to the file
+    inputFile = os.path.join(inputFolder,inputFile)
+
+    # Construct a while loop to iterate through all lines in the datafile
+    # Open the ARGOS data file for reading
+    inputFileObj = open(inputFile,'r')
+
+    # Get the first line of data, so we can use a while loop
     lineString = inputFileObj.readline()
+
+    # Start the while loop
+    while lineString:
     
-#Close the file object
-inputFileObj.close()
+        # Set code to run only if the line contains the string "Date: "
+        if ("Date :" in lineString):
+        
+            # Parse the line into a list
+            lineData = lineString.split()
+        
+            # Extract attributes from the datum header line
+            tagID = lineData[0]
+            obsDate = lineData[3]
+            obsTime = lineData[4]
+            obsClass = lineData[7]
+        
+            # Extract location info from the next line
+            line2String = inputFileObj.readline()
+        
+            # Parse the line into a list
+            line2Data = line2String.split()
+        
+            # Extract the date we need to variables
+            obsLat = line2Data[2]
+            obsLon= line2Data[5]
+        
+            # Print results to see how we're doing
+            # print (tagID,"Date:"+obsDate,"Time:"+obsTime,"Class:"+obsClass,"Lat:"+obsLat,"Long:"+obsLon)
+            #Try to convert the coordinates to numbers
+            try:
+                # Convert raw coordinate strings to numbers
+                if obsLat[-1] == 'N':
+                    obsLat = float(obsLat[:-1])
+                else:
+                    obsLat = float(obsLat[:-1]) * -1
+                if obsLon[-1] == 'E':
+                    obsLon = float(obsLon[:-1])
+                else:
+                    obsLon = float(obsLon[:-1]) * -1
+                # Construct a point object from the feature class
+                obsPoint = arcpy.Point()
+                obsPoint.X = obsLon
+                obsPoint.Y = obsLat
+                # Convert the point to a point geometry object with spatial reference
+                inputSR = arcpy.SpatialReference(4326)
+                obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
+            
+        
+                #Handle any error
+            except Exception as e:
+                arcpy.AddWarning()
+                #print(f"Error adding record {tagID} to the output")
+        # Create a feature object
+        feature = cur.insertRow((obsPointGeom,tagID,obsClass,obsDate.replace(".","/") + " " + obsTime))
+        # Move to the next line so the while loop progresses
+        lineString = inputFileObj.readline()
+    
+    #Close the file object
+    inputFileObj.close()
 #Delete the cursor object
 del cur
